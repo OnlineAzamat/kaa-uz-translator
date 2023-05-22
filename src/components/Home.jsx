2a3c5e9dd0c316e8ef00a452f3555d2794b0f620
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-05-22 22:59:40 +0500

    added change language btn styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,54 @@
-import { Box } from "@mui/material";
-import Textarea from "./Textarea";
-import SelectLang from "./SelectLang";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+
+import { Box } from "@mui/material";
+import { Translate } from "@mui/icons-material";
+
+import Textarea from "./Textarea";
+import SelectLang from "./SelectLang";
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
     <Box sx={{ padding: { xs: "10px" } }}>
-      <ul className="navbar-container">
-        <div className="menu">
-          <div>
-            <span></span>
-            <span></span>
-            <span></span>
+      <div className="navbar-container">
+        <ul className="navbar-items">
+          <div className="menu">
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <Link to="/" className="navbar-item active">
+            <li>
+              {t('home')}
+            </li>
+          </Link>
+          <Link to="/transliteration" className="navbar-item">
+            <li>
+              {t('transliteration')}
+            </li>
+          </Link>
+          <Link to="/v" className="navbar-item">
+            <li>
+              {t('version')}
+            </li>
+          </Link>
+          <a href="#" className="navbar-item">
+            <li>
+              Language
+            </li>
+          </a>
+        </ul>
+
+        <div className="change-lang">
+          <div className="lang-icon">
+            <Translate />
           </div>
         </div>
-        <Link to="/" className="navbar-item active">
-          <li>
-            {t('home')}
-          </li>
-        </Link>
-        <Link to="/transliteration" className="navbar-item">
-          <li>
-            {t('transliteration')}
-          </li>
-        </Link>
-        <Link to="/v" className="navbar-item">
-          <li>
-            {t('version')}
-          </li>
-        </Link>
-        <a href="#" className="navbar-item">
-          <li>
-            Language
-          </li>
-        </a>
-      </ul>
+      </div>
 
       <Box sx={{
         display: 'flex',
