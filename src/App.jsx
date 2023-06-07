e021d79f3f1647678a80248512f4223f4f3824f1
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-06-07 09:26:30 +0500

    addded 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css';
 import Home from "./components/Home";
 import Transliterate from "./components/Transliterate"
 import About from "./components/About";
+import NotFoundPage from "./components/NotFoundPage";
 import i18n from "./lang/i18";
 
 function App() {
-
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/transliterate" element={<Transliterate />} />
           <Route path="/about-us" element={<About />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
   )
